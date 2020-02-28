360a59463b3fac01054d8ac8f33d6f380f730640
MerlinEl (Orien) <merlin_el@hotmail.com>
2020-02-28 14:00:48 +0100

    up

--- C#/Orien.Tools/mcArray.cs
+++ C#/Orien.Tools/mcArray.cs
@@ -4,17 +4,24 @@ using System.Collections.Generic;
 namespace Orien.Tools {
     public static class mcArray {
 
-        public static void AddItemUnique<T>(this List<T> list, T item) {
+        public static void AddItem<T>(this List<T> list, T item, bool unique = false) {
 
-            if ( !list.Contains(item) ) {
+            if (unique && !list.Contains(item) ) {
+                list.Add(item);
+            } else {
+
                 list.Add(item);
             }
+        }
+        public static void Prepend<T>(this List<T> list, T item) {
+
+            list.Insert(0, item);
         }
         public static void TestFn(string s) {
             //manual comments
             //https://docs.microsoft.com/cs-cz/dotnet/csharp/language-reference/language-specification/documentation-comments
             //manual snippet designer
-            // https://github.com/mmanela/SnippetDesigner
+            //https://github.com/mmanela/SnippetDesigner
 
             mcXML doc = new mcXML("c:/temp/text_01.xml");
             Console.WriteLine("XML body:", doc.Body);
@@ -24,9 +31,6 @@ namespace Orien.Tools {
 
             //Add Snippet > 2x Tab
             //multi rename Ctrl+rx2
-            public void Test(string value) {
-                m_Test = value;
-            }
         }
     }
 }
