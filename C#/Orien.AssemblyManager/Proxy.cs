a6d6291ee2c24e196c3d4ed46da4b79088118386
MerlinEl (Orien) <merlin_el@hotmail.com>
2020-05-18 19:59:54 +0200

    up

---
+++ C#/Orien.AssemblyManager/Proxy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace Orien.AssemblyManager {
+    class Proxy : MarshalByRefObject {
+        public Assembly LoadAssembly(string assemblyPath) {
+            try {
+                return Assembly.LoadFrom(assemblyPath);
+            } catch ( Exception ex ) {
+                throw new InvalidOperationException(ex.Message);
+            }
+        }
+    }
+}
